1c3389065c877411d01226195cc3b2e974c14fbc
amitbadheka <ab@pykih.com>
2015-09-16 01:07:00 +0530

    Change path for pid in unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory "/var/www/html/api-rumi-io"
-pid "/var/www/html/api-rumi-io/unicorn.pid" 
+working_directory "/var/www/html/datastory-api"
+pid "/var/www/html/datastory-api/unicorn.pid" 
 stderr_path "/var/log/unicorn.log" 
 stdout_path "/var/log/unicorn.log" 
-listen "/tmp/unicorn.sock"
+listen "/tmp/api-unicorn.sock"
 worker_processes 2 
 timeout 300
