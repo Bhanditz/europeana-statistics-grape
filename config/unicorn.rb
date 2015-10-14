ca51c81f3c2b506d8ebed5e95bfa13537ff33c33
amitbadheka <ab@pykih.com>
2015-10-14 17:28:55 +0530

    Clearing master branch

--- config/unicorn.rb
+++
@@ -1,7 +0,0 @@
-working_directory "/var/www/html/datastory-api"
-pid "/var/www/html/datastory-api/unicorn.pid" 
-stderr_path "/var/log/unicorn.log" 
-stdout_path "/var/log/unicorn.log" 
-listen "/tmp/api-unicorn.sock"
-worker_processes 2 
-timeout 300
