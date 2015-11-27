21f3051fd29e76ba0e1d9b5c4af6af404a82fb3b
amitbadheka <ab@pykih.com>
2015-11-28 02:30:34 +0530

    Changes sock name for unicorn configurations

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ working_directory "/var/www/html/europeana-statistics-grape"
 pid "/var/www/html/europeana-statistics-grape/unicorn.pid"
 stderr_path "/var/log/unicorn-error.log"
 stdout_path "/var/log/unicorn-access.log"
-listen "/tmp/unicorn.sock"
+listen "/tmp/api-unicorn.sock"
 worker_processes 2
 timeout 60
