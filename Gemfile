26c0b6524099346150e19d3b1cfe912977ac9da6
amitbadheka <ab@pykih.com>
2015-11-28 01:27:33 +0530

    Switches the application server to unicorn server

--- Gemfile
+++ Gemfile
@@ -13,3 +13,5 @@ gem 'logging'
 gem 'puma'
 gem 'foreman'
 gem 'dotenv'
+#For Hosting, Once Richard Comes, we can revert it back to puma
+gem "unicorn"
---
+++ config/unicorn.rb
@@ -0,0 +1,7 @@
+working_directory "/var/www/html/europeana-statistics-grape"
+pid "/var/www/html/europeana-statistics-grape/tmp/pids/unicorn.pid"
+stderr_path "/var/www/html/europeana-statistics-grape/log/unicorn.log"
+stdout_path "/var/www/html/europeana-statistics-grape/log/unicorn.log"
+listen "/tmp/unicorn.sock"
+worker_processes 2
+timeout 60
