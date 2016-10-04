2ad1ae5f1ec252613898488699911be1c3251c19
Richard Doe <richard.doe@rwdit.net>
2016-10-04 15:07:45 +0100

    Simplify Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,10 @@
+# frozen_string_literal: true
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-app_dir = File.expand_path("../..", __FILE__)
-
 preload_app!
 
-rackup DefaultRackup
-
-rails_env = ENV['RACK_ENV'] || 'development'
-
-if rails_env == 'development'
-  port ENV['PORT'] || 3000 if rails_env == 'development'
-else
-  bind "unix://#{app_dir}/puma.sock"
-end
-
-environment rails_env
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
